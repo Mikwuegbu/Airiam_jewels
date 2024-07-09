b15d5227ce41bb6000394343f9cf7e81cfb42d1e
Mikwuegbu <mstephen130@yahoo.com>
2024-07-09 12:54:57 +0100

    HeroDesktop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,28 @@ import { heroBg } from '../assets/icons';
 const Home = () => {
 	return (
 		<div className="bg-[#FFF8E7]">
-			<div className="grid justify-center place-items-center text-center px-6 gap-y-3 py-4">
-				<h1 className="font-MerriWeather text-3xl font-bold">
-					Define your style
-					<br /> Shop now, discover.
-				</h1>
-				<p className="font-normal text-sm font-Raleway">
-					Discover curated jewelry essentials that redefine your style. Explore
-					trends, quality craftsmanship, and timeless elegance, all at your
-					fingertips.
-				</p>
-				<button className="font-Raleway text-sm border border-[#C99A9A] rounded-full w-[120px] p-2.5">
-					Shop Now
-				</button>
-				<img src={heroBg} alt={heroBg} className="w-[249px] h-[283px]" />
+			<div className="grid justify-center place-items-center lg:text-left text-center px-6 gap-y-3 lg:py-0 py-4 lg:grid-flow-col">
+				<div className="space-y-3 lg:space-y-9 max-w-[624px]">
+					<h1 className="font-MerriWeather text-3xl lg:text-5xl font-bold text-nowrap">
+						Define your style
+						<br /> Shop now, discover.
+					</h1>
+					<p className="font-normal text-sm lg:text-2xl font-Raleway">
+						Discover curated jewelry essentials that redefine your style.
+						Explore trends, quality craftsmanship, and timeless elegance, all at
+						your fingertips.
+					</p>
+					<button className="font-Raleway text-sm lg:text-2xl border border-[#C99A9A] rounded-full w-[120px] lg:w-[267px] p-2.5 lg:p-3">
+						Shop Now
+					</button>
+				</div>
+				<div className="">
+					<img
+						src={heroBg}
+						alt={heroBg}
+						className="w-[249px] h-[283px] lg:w-[612px] lg:h-[696px] "
+					/>
+				</div>
 			</div>
 		</div>
 	);
